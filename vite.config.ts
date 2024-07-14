3c5c7b1b80af6ae1d2e94f5feb1c990059afb804
adelsultann <maax.1992@gmail.com>
2024-07-14 21:24:29 +0300

    deployment 3 try

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+      plugins: [tailwindcss, autoprefixer],
     },
   },
 });
