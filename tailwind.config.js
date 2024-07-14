d5b565041f44db4e13cff4085690fe18327e447c
adelsultann <maax.1992@gmail.com>
2024-07-14 20:56:08 +0300

    deployment 3 try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 // tailwind.config.js
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
