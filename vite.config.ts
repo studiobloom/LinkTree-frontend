4032774274712fbeed39b76ea0855e3717037332
adelsultann <maax.1992@gmail.com>
2024-07-15 02:29:44 +0300

    deployment 3 try

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,15 +11,9 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  },
-  preview: {
-    port: 3000,
-    strictPort: true,
   },
 });
