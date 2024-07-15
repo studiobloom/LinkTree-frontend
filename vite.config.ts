aeb03c90ea0d7e7da37d5017aad10014bb097030
adelsultann <maax.1992@gmail.com>
2024-07-15 03:07:39 +0300

    deployment 3 try

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
     },
   },
+  build: {
+    outDir: 'dist', // Ensure this matches the directory Render is looking for
+  },
+ 
 });
